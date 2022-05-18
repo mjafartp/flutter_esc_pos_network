5fbfb8201908aac4ea0a7f6be99ff87b2e50d1a1
jafar <jafar@lithos.in>
2022-05-18 12:06:19 +0530

    Ver. 1.0.0

--- lib/src/network_printer.dart
+++ lib/src/network_printer.dart
@@ -4,6 +4,7 @@ import 'enums.dart';
 class PrinterNetworkManager {
   late String _host;
   int _port = 9100;
+  bool _isConnected = false;
   Duration _timeout = const Duration(seconds: 5);
   late Socket _socket;
 
@@ -17,35 +18,41 @@ class PrinterNetworkManager {
     _timeout = timeout;
   }
 
-  Future<PosPrintResult> connect() async {
+  Future<PosPrintResult> connect(
+      {Duration? timeout: const Duration(seconds: 5)}) async {
     try {
       _socket = await Socket.connect(_host, _port, timeout: _timeout);
+      _isConnected = true;
+      return Future<PosPrintResult>.value(PosPrintResult.success);
+    } catch (e) {
+      _isConnected = false;
+      return Future<PosPrintResult>.value(PosPrintResult.timeout);
+    }
+  }
+
+  Future<PosPrintResult> printTicket(List<int> data,
+      {bool isDisconnect = true}) async {
+    try {
+      if (!_isConnected) {
+        await connect();
+      }
+      _socket?.add(data);
+      if (isDisconnect) {
+        await disconnect();
+      }
       return Future<PosPrintResult>.value(PosPrintResult.success);
     } catch (e) {
       return Future<PosPrintResult>.value(PosPrintResult.timeout);
     }
   }
 
-  Future<PosPrintResult> printTicket(List<int> ticket) async {
-    try {
-      if (ticket.isNotEmpty) {
-        _socket.add(ticket);
-        return Future<PosPrintResult>.value(PosPrintResult.success);
-      }
-      return Future<PosPrintResult>.value(PosPrintResult.ticketEmpty);
-    } catch (e) {
-      return Future<PosPrintResult>.value(PosPrintResult.timeout);
+  Future<PosPrintResult> disconnect({Duration? timeout}) async {
+    await _socket.flush();
+    await _socket.close();
+    _isConnected = false;
+    if (timeout != null) {
+      await Future.delayed(timeout, () => null);
     }
-  }
-
-  void disconnect({int delayMs = 0}) async {
-    try {
-      _socket.destroy();
-      if (delayMs > 0) {
-        await Future.delayed(Duration(milliseconds: delayMs), () => null);
-      }
-    } catch (e) {
-      return;
-    }
+    return Future<PosPrintResult>.value(PosPrintResult.success);
   }
 }
